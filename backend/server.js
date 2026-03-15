ffe443e7a5eeddd42572d8d95698d9753ff27a70
daniel <ddny3622@gmail.com>
2026-03-15 10:33:19 +0200

    add connect MONGO_DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv"
-const PORT = process.env.PORT
-dotenv.config()
+import dotenv from "dotenv";
+import { connectToMongoDB } from "./DB/conectNongoDB.js";
+dotenv.config();
+
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
@@ -12,11 +14,11 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-
 app.get("/api", (req, res) => {
   res.status(200).json("Welcome ");
 });
 
 app.listen(PORT, () => {
-  console.log("server is running on port 5000");
+  connectToMongoDB();
+  console.log(`server is running on port ${PORT}`);
 });
